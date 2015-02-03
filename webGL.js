0aa375d3a5afdb55ddb5072e2caf4a4afcb30897
Chris VZ <chris.vonzuben@gmail.com>
2015-02-02 23:39:47 -0500

    added glShader object and soame shaders plus minor commenting

--- webGL.js
+++ webGL.js
@@ -1,4 +1,5 @@
 
+// takes gl context, draw target, and buffer usage parameter
 function glBuffer(gl, target, usage) {
     this.buffPtr = gl.createBuffer();
     this.gl = gl;
@@ -12,11 +13,36 @@ function glBuffer(gl, target, usage) {
         gl.bindBuffer(this.target, this.buffPtr);
     }
 
+    //data is the data to put on the gpu, needs to be ArrayBuffer eg Float32Array
     this.bufferData = function(data) {
         this.bindBuffer();
         this.gl.bufferData(this.target, data, this.usage);
     }
 
+}
+
+//takes gl context and a list of shader source strings to be compiled onto the program
+function glShaderProgram(gl, vertSrc, fragSrc){
+    this.prgm = gl.createProgram();
+    this.gl = gl;
+    this.vertSrc = vertSrc;
+    this.fragSrc = fragSrc;
+    this.vertShdr = gl.createShader(gl.VERTEX_SHADER);
+    this.fragShdr = gl.createShader(gl.FRAGMENT_SHADER);
+
+    this.initPrgm = function () {
+        this.gl.shaderSource(this.vertShdr, this.vertSrc);
+        this.gl.shaderSource(this.fragShdr, this.fragSrc);
+        this.gl.compileShader(this.vertShdr);
+        this.gl.compileShader(this.fragShdr);
+        this.gl.attachShader(this.prgm, this.vertShdr);
+        this.gl.attachShader(this.prgm, this.fragShdr);
+        this.gl.linkProgram(this.prgm);
+    }
+
+    this.usePrgm = function () {
+        this.gl.usePrgmogram(this.prgm);
+    }
 }
 
 function initBuffer(gl) {
@@ -42,5 +68,10 @@ function main(){
 
     gl = initGL(canv);
 
+    //testing
     vertBuf = new glBuffer(gl, gl.ARRAY_BUFFER, gl.STATIC_DRAW);
+
+    vs = document.getElementById("vertSrc").innerHTML;
+    fs = document.getElementById("fragSrc").innerHTML;
+    shdr = new glShaderProgram(gl, vs, fs);
 }
