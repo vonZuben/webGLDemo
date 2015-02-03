aacc2ced3919696afc40d784b3abef3c2dc4b5ef
Chris VZ <chris.vonzuben@gmail.com>
2015-02-02 21:50:40 -0500

    good GL init function

--- webGL.js
+++ webGL.js
@@ -2,6 +2,13 @@
 // set up the WebGL context
 function initGL(canvas) {
     var gl = canvas.getContext("experimental-webgl");
+
+    if (gl == null){
+        alert("no webgl suport");
+        return null;
+    }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
     return gl;
 }
 
