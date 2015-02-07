c3fc5680b33c1861b4c79464226a686d24eeca5f
Chris VZ <chris.vonzuben@gmail.com>
2015-02-07 18:17:37 -0500

    move demos to their own files

---
+++ simpleGL.js
@@ -0,0 +1,53 @@
+function getVertSh() {
+    return "\
+        attribute vec3 pos;\
+            \
+        void main() {\
+            gl_Position = vec4(pos, 1.0);\
+        }\
+    ";
+}
+
+function getFragSh() {
+    return "\
+        void main() {\
+            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\
+        }\
+    ";
+}
+
+function simpleGL(gl) {
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // verts for a triangle
+    vertData = [
+         0.0,  1.0, 0.0,
+        -1.0, -1.0, 0.0,
+         1.0, -1.0, 0.0
+            ];
+
+    // shader program sources
+    vs = getVertSh();
+    fs = getFragSh();
+
+    // make the shader program and be sure to usePrgm()
+    simpleShdr = new glShaderProgram(gl, vs, fs);
+    simpleShdr.initPrgm();
+    simpleShdr.usePrgm();
+
+    // make vertex buffer for shader program attribute needs
+    // need to bind the buffer but will happen automatically on bufferData()
+    vertBuf = new glBuffer(gl, gl.ARRAY_BUFFER, gl.STATIC_DRAW);
+    vertBuf.bindBuffer();
+    vertBuf.bufferData(new Float32Array(vertData));
+
+    // enable shader program vertex attribute for the verts
+    // and make it point into the buffer appropriately
+    att = simpleShdr.enableVAA("pos")
+    gl.vertexAttribPointer(att, 3, gl.FLOAT, false, 12, 0);
+
+    // clear and draw to the bound frameBuffer (screen by default)
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
--- webGL.js
+++ webGL.js
@@ -120,42 +120,6 @@ function initGL(canvas) {
     return gl;
 }
 
-function simpleGL(gl) {
-
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-    // verts for a triangle
-    vertData = [
-         0.0,  1.0, 0.0,
-        -1.0, -1.0, 0.0,
-         1.0, -1.0, 0.0
-            ];
-
-    // shader program sources
-    vs = document.getElementById("vertSrc").innerHTML;
-    fs = document.getElementById("fragSrc").innerHTML;
-
-    // make the shader program and be sure to usePrgm()
-    simpleShdr = new glShaderProgram(gl, vs, fs);
-    simpleShdr.initPrgm();
-    simpleShdr.usePrgm();
-
-    // make vertex buffer for shader program attribute needs
-    // need to bind the buffer but will happen automatically on bufferData()
-    vertBuf = new glBuffer(gl, gl.ARRAY_BUFFER, gl.STATIC_DRAW);
-    vertBuf.bindBuffer();
-    vertBuf.bufferData(new Float32Array(vertData));
-
-    // enable shader program vertex attribute for the verts
-    // and make it point into the buffer appropriately
-    att = simpleShdr.enableVAA("pos")
-    gl.vertexAttribPointer(att, 3, gl.FLOAT, false, 12, 0);
-
-    // clear and draw to the bound frameBuffer (screen by default)
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-}
-
 function main(){
     var canv = document.getElementById("canv");
 
