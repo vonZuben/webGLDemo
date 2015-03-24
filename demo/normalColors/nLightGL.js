df3de0db4865ca304fd150469377b26574f84ff1
Chris VZ <chris.vonzuben@gmail.com>
2015-03-24 00:12:38 -0400

    remove normal style toggle and test new buffer packing style

--- demo/normalColors/nLightGL.js
+++ demo/normalColors/nLightGL.js
@@ -15,13 +15,7 @@ function nLightGL(canvas) {
     loader.loadFile("frag.fs", fsrc);
 
     var bufVert = new glBuffer(gl, gl.ARRAY_BUFFER, gl.STATIC_DRAW);
-    var bufNorm = new glBuffer(gl, gl.ARRAY_BUFFER, gl.STATIC_DRAW);
     var bufElem = new glBuffer(gl, gl.ELEMENT_ARRAY_BUFFER, gl.STATIC_DRAW);
-
-    // to alternate between flat and smooth shading
-    // arrays for flat and smooth normals
-    var normalsFlat;
-    var normalsSmooth;
 
     var draw = function () {
         requestAnimationFrame(draw);
@@ -29,7 +23,8 @@ function nLightGL(canvas) {
         mat4.rotateY(rot, 0.01);
         mat4.rotateX(rot, 0.005);
         gl.uniformMatrix4fv(rotLoc, false, rot);
-        gl.drawElements(gl.TRIANGLES, obj.numVerts(), gl.UNSIGNED_SHORT, 0);
+        //gl.drawElements(gl.TRIANGLES, obj.numVerts(), gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, 1700, gl.UNSIGNED_SHORT, 0);
     }
 
     var persp = mat4.create();
@@ -54,34 +49,23 @@ function nLightGL(canvas) {
         // only have one buffer for each target so dont have to wory about which one is bound
         // bufferData call from glBuffer does it for me
         var verts = obj.vertexArray();
-        normalsSmooth = obj.normalArray(); // this is just the normals from the file which happen to be smooth
-        normalsFlat = obj.calcFlatNormals();
+        var normalsSmooth = obj.normalArray(); // this is just the normals from the file which happen to be smooth
         var elements = obj.elements();
 
-        bufVert.bufferData(verts);
-        gl.vertexAttribPointer(shdr.enableVAA("pos"), 3, gl.FLOAT, false, 0, 0);
+        var vertData = [];
+        var numVerts = obj.numVerts();
+        for (var i = 0; i < numVerts; i += 3) {
+            vertData = vertData.concat( [ verts[ i + 0 ], verts[ i + 1 ], verts[ i + 2 ], normalsSmooth[ i + 0 ], normalsSmooth[ i + 1 ], normalsSmooth[ i + 2 ] ] );
+        }
 
-        bufNorm.bufferData(normalsSmooth);
-        gl.vertexAttribPointer(shdr.enableVAA("normal"), 3, gl.FLOAT, false, 0, 0);
+        //var vertData = verts.reduce( function (accumulator, vertex, i) {
+        //    return accumulator.concat(vertex.concat(normalsSmooth[i]));
+        //}, []);
+        bufVert.bufferData(new Float32Array(vertData));
+        gl.vertexAttribPointer(shdr.enableVAA("pos"), 3, gl.FLOAT, false, 24, 0);
+        gl.vertexAttribPointer(shdr.enableVAA("normal"), 3, gl.FLOAT, false, 24, 12);
 
-        // set up function to alternate between flat and smooth shading on mouse click
-        toggleNormals = function () {
-            var toggle = 0; // 0 is smooth and 1 is flat
-            return function () {
-                if (toggle == 0) {
-                    bufNorm.bufferData(normalsFlat);
-                    toggle = 1;
-                }
-                else {
-                    bufNorm.bufferData(normalsSmooth);
-                    toggle = 0;
-                }
-            };
-        };
-
-        gl.canvas.onclick = toggleNormals();
-
-        bufElem.bufferData(elements);
+        bufElem.bufferData(new Uint16Array(elements));
 
         gl.uniformMatrix4fv(shdr.getuLoc("persp"), false, persp);
         gl.uniformMatrix4fv(shdr.getuLoc("trans"), false, trans);
